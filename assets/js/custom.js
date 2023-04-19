7a585d3287e4f05478c67ac527cf57b45cdc62d0
nguyenvandieu <nguyenvandieu@luvina.net>
2023-04-19 14:38:36 +0700

    code day 5

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,6 +57,25 @@
 			  }
 		 }
 	 })
+	 
+	 $('.owl-gen-item').owlCarousel({
+		items:5,
+		loop:true,
+		dots: true,
+		nav: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:2
+			  },
+			  1000:{
+				  items:3
+			  }
+		 }
+	 })
 
 	$(window).scroll(function() {
 	  var scroll = $(window).scrollTop();
